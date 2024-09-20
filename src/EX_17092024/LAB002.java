8d27d5a07345502fec02af37f4543ed242af294c
Ayushistella <ayushisrivastava12as@gmail.com>
2024-09-20 20:23:58 +0530

    HELLO

--- src/EX_17092024/LAB002.java
+++ src/EX_17092024/LAB002.java
@@ -1,4 +1,9 @@
 package EX_17092024;
 
 public class LAB002 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
 }
---
+++ src/EX_17092024/LAB003.java
@@ -0,0 +1,8 @@
+package EX_17092024;
+
+public class LAB003 {
+    public static void main(String[] args) {
+        System.out.println("Hello Sella");
+
+    }
+}
